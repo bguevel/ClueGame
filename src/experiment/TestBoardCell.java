8373191e2d77307de43988026b13ec880214c590
bguevel <bguevel@mines.edu>
2023-10-05 11:55:56 -0600

    added first functionality for the TeadBoardCell class

---
+++ src/experiment/TestBoardCell.java
@@ -0,0 +1,30 @@
+package experiment;
+
+import java.util.Set;
+
+public class TestBoardCell{
+	private int row;
+	private int column;
+	private Set<TestBoardCell> adjList;
+	private boolean room;
+	private boolean occupied;
+	public TestBoardCell(int row, int col) {
+		this.row=row;
+		column = col;
+	}
+	public void addAdjacency(TestBoardCell cell) {
+		adjList.add(cell);
+	}
+	public void setRoom(boolean b){
+		room=b;
+	}
+	public boolean isRoom() {
+		return room;
+	}
+	public void setOccupation(boolean b) {
+		occupied =b;
+	}
+	public boolean getOccupation() {
+		return occupied;
+	}
+}
