decf7b3fe864e24f39b628038448b8e1e7d40f3e
bguevel <bguevel@mines.edu>
2023-10-10 00:47:04 -0600

    maybe all that has to be done with the exception class

--- src/experiment/BadConfigFormatException.java
+++ src/experiment/BadConfigFormatException.java
@@ -1,5 +1,14 @@
 package experiment;
 
 public class BadConfigFormatException extends Exception{
+	String file;
+	public BadConfigFormatException(String fl) {
+		super("Error: bad config format");
+		file =fl;
+	}
+	@Override
+	public String toString() {
+		return "BadConfigFormatException [file= " + file + "]";
+	}
 	
 }
