d8d641ef7ebe8853cbf587bb28f7fcc4dfd5b3c3
bguevel <bguevel@mines.edu>
2023-11-05 14:46:45 -0700

    disprovesuggestion logic added seems to be correct.

--- src/clueGame/Player.java
+++ src/clueGame/Player.java
@@ -2,6 +2,7 @@ package clueGame;
 
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.function.BooleanSupplier;
 
 public abstract class Player {
@@ -22,7 +23,33 @@ public abstract class Player {
 	}
 	
 	public Card disproveSuggestion(Solution suggestion) {
-		return null;
+		// need random logic
+		ArrayList<Card> involved = new ArrayList<Card>();
+		boolean foundP =false;
+		boolean foundR =false;
+		boolean foundW =false;
+		for(Card c: hand) {
+			if(c.equals(suggestion.getPerson()) && foundP == false) { // check to see if person in suggestion is in player's hand
+				involved.add(c);
+				foundP=true;
+			}
+			if(c.equals(suggestion.getRoom()) && foundR == false) { // check to see if room in suggestion is in player's hand
+				involved.add(c);
+				foundR =true;
+			}
+			if(c.equals(suggestion.getWeapon()) && foundW == false) {// check to see if weapon in suggestion is in player's hand
+				involved.add(c);
+				foundW=true;
+			}
+		}
+		if(involved.size()>1) { // if there are more than 2 cards from the suggestion present in the player's hand then we return a rand one
+			Random rand = new Random();
+			return this.hand.get(rand.nextInt(2 +1));
+		}else if(involved.size() == 1){ // should only be one thing in the arraylist at idx 0
+			return involved.get(0);
+		}else { // if there is nothing from the suggestion in the player's hand
+			return null;
+		}
 	}
 	
 	public void updateHand(Card card) {
@@ -59,6 +86,5 @@ public abstract class Player {
 	public  Boolean getIsHuman() {
 		return isHuman;
 	}
-
 	
 }
