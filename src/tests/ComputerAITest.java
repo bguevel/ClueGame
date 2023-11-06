7e69a29fe04cafa69dcaaf7ac518ca8336e3ef33
Kaelyn Boutin <kvboutin@mines.edu>
2023-11-06 14:46:18 -0700

    computeraitests made

---
+++ src/tests/ComputerAITest.java
@@ -0,0 +1,126 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+import clueGame.Board;
+import clueGame.BoardCell;
+import clueGame.Card;
+import clueGame.CardType;
+import clueGame.Solution;
+
+public class ComputerAITest {
+	private static Board board;
+	private static Card marquezCard, ctlmCard, brownCard, strongCard, cpwCard, swansonCard, bridgemanCard, canCard, kellyCard, catapultCard, flamethrowerCard, legoCard, butterknifeCard, textbookCard, chairCard;
+
+
+	@BeforeAll
+	public static void setUp() {
+		// Board is singleton, get the only instance
+		board = Board.getInstance();
+		// set the file names to use my config files
+		board.setConfigFiles("data/ClueLayout.csv", "data/ClueSetup.txt");
+		// Initialize will load BOTH config files
+		board.initialize();
+
+		//create card constants to test, 3 of each card type to start
+		marquezCard = new Card("Marquez", CardType.ROOM);
+		ctlmCard = new Card("CTLM", CardType.ROOM);
+		brownCard = new Card("Brown", CardType.ROOM);
+		strongCard = new Card("Prof Strong", CardType.PERSON);
+		cpwCard = new Card("Prof CPW", CardType.PERSON);
+		kellyCard = new Card("Prof Kelly", CardType.PERSON);
+		swansonCard = new Card("Prof Swanson", CardType.PERSON);
+		bridgemanCard = new Card("Prof Bridgeman", CardType.PERSON);
+		canCard = new Card("Prof Can", CardType.PERSON);
+		catapultCard = new Card("Catapult", CardType.WEAPON);
+		flamethrowerCard = new Card("Flamethrower", CardType.WEAPON);
+		legoCard = new Card("Lego", CardType.WEAPON);
+		butterknifeCard = new Card("Butterknife", CardType.WEAPON);
+		chairCard = new Card("Chair", CardType.WEAPON);
+		textbookCard = new Card("Textbook", CardType.WEAPON);
+	}
+	
+	@Test
+	public void testSelectTargets() {
+		//test room has not been seen
+		board.getPlayer("Prof CPW").setLocation(board.getCell(5,  3));
+		board.calcTargets(board.getCell(5, 3), 3);
+		Set<BoardCell> targets = board.getTargets();
+		assertEquals(board.getCell(3, 4), board.getPlayer("Prof CPW").selectTargets(targets));
+		
+		//test room that has been seen
+		board.getPlayer("Prof CPW").updateSeen(ctlmCard);
+		board.calcTargets(board.getCell(5, 3), 3);
+		targets = board.getTargets();
+		
+		ArrayList<BoardCell> testList = new ArrayList<BoardCell>();
+		while(testList.size() != 7) {
+			BoardCell returnedCell = board.getPlayer("Prof CPW").selectTargets(targets);
+			if(!testList.contains(returnedCell)) {
+				testList.add(returnedCell);
+			}
+		}
+		assertTrue(testList.contains(board.getCell(5, 2)));
+		assertTrue(testList.contains(board.getCell(6, 3)));
+		assertTrue(testList.contains(board.getCell(5, 6)));
+		assertTrue(testList.contains(board.getCell(5, 0)));
+		assertTrue(testList.contains(board.getCell(6, 5)));
+		assertTrue(testList.contains(board.getCell(5, 4)));
+		assertTrue(testList.contains(board.getCell(6, 1)));
+		assertFalse(testList.contains(board.getCell(3, 4)));
+		
+		//test no room in list
+		board.getPlayer("Prof CPW").setLocation(board.getCell(0, 0));
+		board.calcTargets(board.getCell(5, 3), 3);
+		targets = board.getTargets();
+		testList.clear();
+		while(testList.size() != 2) {
+			BoardCell returnedCell = board.getPlayer("Prof CPW").selectTargets(targets);
+			if(!testList.contains(returnedCell)) {
+				testList.add(returnedCell);
+			}
+		}
+		assertTrue(testList.contains(board.getCell(3, 0)));
+		assertTrue(testList.contains(board.getCell(0, 3)));
+		
+	}
+	
+	@Test
+	public void testCreateSuggestion() {
+		board.getPlayer("Prof CPW").updateSeen(strongCard);
+		board.getPlayer("Prof CPW").updateSeen(cpwCard);
+		board.getPlayer("Prof CPW").updateSeen(kellyCard);
+		board.getPlayer("Prof CPW").updateSeen(swansonCard);
+		board.getPlayer("Prof CPW").updateSeen(catapultCard);
+		board.getPlayer("Prof CPW").updateSeen(flamethrowerCard);
+		board.getPlayer("Prof CPW").updateSeen(butterknifeCard);
+		board.getPlayer("Prof CPW").updateSeen(legoCard);
+		
+		ArrayList<Solution> testList = new ArrayList<Solution>();
+		while(testList.size() != 4) {
+			Solution returnedSol = board.getPlayer("Prof CPW").createSuggestion(ctlmCard);
+			if(!testList.contains(returnedSol)) {
+				testList.add(returnedSol);
+			}
+		}
+		assertTrue(testList.contains(new Solution(ctlmCard, bridgemanCard, textbookCard)));
+		assertTrue(testList.contains(new Solution(ctlmCard, bridgemanCard, chairCard)));
+		assertTrue(testList.contains(new Solution(ctlmCard, canCard, textbookCard)));
+		assertTrue(testList.contains(new Solution(ctlmCard, canCard, chairCard)));
+		
+		board.getPlayer("Prof CPW").updateSeen(bridgemanCard);
+		board.getPlayer("Prof CPW").updateHand(chairCard);
+		
+		assertEquals(new Solution(ctlmCard, canCard, textbookCard), board.getPlayer("Prof CPW").createSuggestion(ctlmCard));
+		
+		
+	}
+}
---
+++ src/tests/GameSolutionTest.java
@@ -0,0 +1,107 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+import clueGame.Board;
+import clueGame.Card;
+import clueGame.CardType;
+import clueGame.Solution;
+
+public class GameSolutionTest {
+	private static Board board;
+	private static Card marquezCard, ctlmCard, brownCard, kafadarCard, strongCard, cpwCard, kellyCard, swansonCard, bridgemanCard, canCard, catapultCard, flamethrowerCard, legoCard, chairCard;
+
+
+	@BeforeAll
+	public static void setUp() {
+		// Board is singleton, get the only instance
+		board = Board.getInstance();
+		// set the file names to use my config files
+		board.setConfigFiles("data/ClueLayout.csv", "data/ClueSetup.txt");
+		// Initialize will load BOTH config files
+		board.initialize();
+
+		//create card constants to test
+		marquezCard = new Card("Marquez", CardType.ROOM);
+		ctlmCard = new Card("CTLM", CardType.ROOM);
+		brownCard = new Card("Brown", CardType.ROOM);
+		kafadarCard = new Card("Kafadar", CardType.ROOM);
+		strongCard = new Card("Prof Strong", CardType.PERSON);
+		cpwCard = new Card("Prof CPW", CardType.PERSON);
+		kellyCard = new Card("Prof Kelly", CardType.PERSON);
+		swansonCard = new Card("Prof Swanson", CardType.PERSON);
+		bridgemanCard = new Card("Prof Bridgeman", CardType.PERSON);
+		canCard = new Card("Prof Can", CardType.PERSON);
+		catapultCard = new Card("Catapult", CardType.WEAPON);
+		flamethrowerCard = new Card("Flamethrower", CardType.WEAPON);
+		legoCard = new Card("Lego", CardType.WEAPON);
+		chairCard = new Card("Chair", CardType.WEAPON);
+	}
+	
+	@Test
+	public void testCheckAccusation() {
+		board.setTheAnswer(marquezCard, strongCard, legoCard);
+		//correct answer
+		assertTrue(board.checkAccusation(new Solution(marquezCard, strongCard, legoCard)));
+		//wrong room
+		assertFalse(board.checkAccusation(new Solution(ctlmCard, strongCard, legoCard)));
+		//wrong person
+		assertFalse(board.checkAccusation(new Solution(marquezCard, kellyCard, legoCard)));
+		//wrong weapon
+		assertTrue(board.checkAccusation(new Solution(marquezCard, strongCard, flamethrowerCard)));
+	}
+	
+	@Test
+	public void testDisproveSuggestion() {
+		Solution suggestion = new Solution(marquezCard, strongCard, legoCard);
+		
+		//player has 1 matching card
+		board.getPlayer("Prof Strong").setHand(marquezCard, cpwCard, flamethrowerCard);
+		assertEquals(marquezCard, board.getPlayer("Prof Strong").disproveSuggestion(suggestion));
+		
+		//player has no matching cards
+		board.getPlayer("Prof Strong").setHand(ctlmCard, kellyCard, brownCard);
+		assertEquals(null, board.getPlayer("Prof Strong").disproveSuggestion(suggestion));
+		
+		//player has more than 1 (2) matching cards
+		board.getPlayer("Prof Strong").setHand(marquezCard, strongCard, flamethrowerCard);
+		ArrayList<Card> testList = new ArrayList<Card>();
+		while(testList.size() != 2) {
+			Card returnedCard = board.getPlayer("Prof Strong").disproveSuggestion(suggestion);
+			if(!testList.contains(returnedCard)) {
+				testList.add(returnedCard);
+			}
+		}
+		assertTrue(testList.contains(marquezCard));
+		assertTrue(testList.contains(strongCard));
+	}
+	
+	@Test
+	public void testHandleSuggestion() {
+		//setting these three hands to test
+		board.getPlayer("Prof Strong").setHand(ctlmCard, strongCard, flamethrowerCard);
+		board.getPlayer("Prof CPW").setHand(brownCard, kellyCard, catapultCard);
+		board.getPlayer("Prof Kelly").setHand(kafadarCard, swansonCard, chairCard);
+		//setting the rest of their hands to be the same so they don't affect the test from deal being called
+		board.getPlayer("Prof Swanson").setHand(brownCard, canCard, chairCard);
+		board.getPlayer("Prof Bridgeman").setHand(brownCard, canCard, chairCard);
+		board.getPlayer("Prof Can").setHand(brownCard, canCard, chairCard);
+		
+		//suggestion no one can disprove return null
+		Solution suggestion = new Solution(marquezCard, cpwCard, legoCard); 
+		assertEquals(null, board.handleSuggestion(suggestion));
+		//suggestion only suggesting player can disprove return null
+		suggestion = new Solution(ctlmCard, cpwCard, legoCard); //only the human player has ctml
+		assertEquals(null, board.handleSuggestion(suggestion));
+		//suggestion only human player can disprove return card found
+		
+		//suggestion two players can disprove returns first card found
+	}
+}
