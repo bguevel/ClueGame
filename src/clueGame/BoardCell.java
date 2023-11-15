6edf187ad6ab76d290b39e81987b3f3a63f44755
Kaelyn Boutin <kvboutin@mines.edu>
2023-11-15 11:35:47 -0700

    doors look better

--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -38,18 +38,18 @@ public class BoardCell{
 			if(door == true) {
 				switch(doorDirection) {
 				case DOWN:
-					int doorY = yOffset + cellHeight/3 * 2 + cellHeight % 3;
+					int doorY = yOffset + cellHeight/3 * 2 + cellHeight % 3 + 4;
 					g.fillRect(xOffset, doorY, cellWidth, cellHeight/6);
 					break;
 				case RIGHT:
-					int doorX = xOffset + cellWidth/3 * 2 + cellWidth % 3;
+					int doorX = xOffset + cellWidth/3 * 2 + cellWidth % 3 + 10;
 					g.fillRect(doorX, yOffset, cellWidth/6, cellHeight);
 					break;
 				case UP:
 					g.fillRect(xOffset, yOffset, cellWidth, cellHeight/6);
 					break;
 				case LEFT:
-					g.fillRect(xOffset, yOffset, cellWidth/6, cellHeight);
+					g.fillRect(xOffset, yOffset, cellWidth/8, cellHeight);
 					break;
 				default:
 					break;
